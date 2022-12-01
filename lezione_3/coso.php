9815b371b1e66da83c75000da53dec92bf6fb8b0
malubi <119335949+malubi@users.noreply.github.com>
2022-12-01 18:42:17 +0100

    3

--- lezione_3/coso.php
+++ lezione_3/coso.php
@@ -1,5 +1,37 @@
-<?php
-$cars = array("Volvo", "BMW", "Toyota");
-var_dump($cars); 
-echo $cars[2];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+    <h1>Automobili</h1>
+    <p><?php
+    $cars = array("Volvo", "BMW", "Toyota");
+    //var_dump($cars); 
+    echo "Oggi ho comprato una " . $cars[2] . " nuova";
+    ?></p>   
+
+    <p><?php 
+
+    $anni = array("Mario"=> 38, "Luisa" => 22, "Gianni" => 7);
+    //var_dump($anni); 
+    echo $anni['Mario'];
+    ?></p>
+
+    <p><?php
+
+    /*$famiglia = array(
+        "Rossi" => array("A", "B", "C");
+        "Bianchi" => array("D", "E", "F");
+        "verdi" => array("G", "H", "I");
+    );
+
+    var_dump($famiglia); 
+    */?>
+    </p>
+
+</body>
+</html>
